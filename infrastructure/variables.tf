49d7d8e4998a3cbb2516122b218a8cfeb9404993
Adwaid Krishna K <adwaidkrishna44@gmail.com>
2026-06-27 02:30:15 +0530

    feat: provision Airflow scheduler and webserver via Terraform (#15)

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -9,3 +9,21 @@ variable "minio_root_password" {
     type = string
     sensitive = true
 }
+
+variable "airflow_postgres_password" {
+    description = "Password for Airflow's metadata Postgres database"
+    type = string
+    sensitive = true
+}
+
+variable "airflow_fernet_key" {
+    description = "Fernet key Airflow uses to encrypt sensitive data (connections, variables) in its metadata DB"
+    type = string
+    sensitive = true
+}
+
+variable "airflow_admin_password" {
+    description = "Password for the Airflow webserver admin user"
+    type = string
+    sensitive = true
+}
